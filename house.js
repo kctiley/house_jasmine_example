4368283d6965b0647ff5167e544b09086265d27a
Klint Tiley <kctiley@gmail.com>
2015-08-07 11:20:53 -0600

    All passing!

--- house.js
+++ house.js
@@ -1,5 +1,12 @@
-function House(numberOfDoors) {
+function House(numberOfDoors, numberOfWindows) {
     this.numberOfDoors = numberOfDoors;
+    this.numberOfWindows = numberOfWindows;
+    this.toString = function() {
+      var pluralDoors = (this.numberOfDoors > 1) || (this.numberOfDoors) === 0 ? 's': '';
+      var pluralWindows = (this.numberOfWindows > 1) || (this.numberOfWindows) === 0 ? 's': '';
+      return 'The house has ' + this.numberOfDoors + ' door' + pluralDoors +' and ' + this.numberOfWindows + ' window' + pluralWindows;
+    }
+
 
   }
 
